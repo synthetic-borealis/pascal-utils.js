01038a84c707674c840f1a5470c71e2243ca4258
Frazonar <snip.coloured@gmail.com>
2022-08-25 13:37:46 +0300

    feat: Implement compilation

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -1,3 +1,5 @@
+const fs = require('fs/promises');
+const process = require('node:process');
 const pascalUtils = require('../lib');
 
 describe('Compiler detection', () => {
@@ -5,4 +7,18 @@ describe('Compiler detection', () => {
     .then(() => {
       expect(true).toBeTruthy();
     }));
+});
+
+describe('Compilation', () => {
+  const exeExtension = process.platform === 'win32' ? '.exe' : '';
+  const sourceFile = './assets/hello.pas';
+  const exeName = `./hello${exeExtension}`;
+  it('Compiles and links', () => pascalUtils.compile(sourceFile, exeName)
+    .then(() => {
+      expect(true).toBeTruthy();
+    }));
+  afterAll(() => Promise.all([
+    fs.unlink('./hello.o'),
+    fs.unlink(exeName),
+  ]));
 });
